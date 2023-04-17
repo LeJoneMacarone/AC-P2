b99a5d2c1fc1378f66121e2098b485ae39509c4a
Pedro <jpcoutinhoalves@gmail.com>
2023-04-17 14:37:41 +0100

    Endereçamento de (alguns) menus na memória

---
+++ Programa.asm
@@ -0,0 +1,46 @@
+;produtos
+
+;menus
+
+place 2000h 
+MenuInicio:
+string "----------------"
+string "MAQUINA MADEIRA "
+string "   BEM-VINDO    "
+string "----------------"
+string "1) Produtos     "
+string "2) Stock        "
+string "----------------"
+
+MenuCategoria:
+string "----------------"
+string "   Categoria    "
+string "----------------"
+string "1) Bebidas      "
+string "2) Snacks       "
+string "7) Cancelar     "
+string "----------------"
+
+MenuErro:
+string "                "
+string "                "
+string "    ATENCAO     "
+string "     OPCAO      "
+string "    ERRADA      "
+string "                "
+string "                "
+
+MenuStock:
+string "     Stock      "
+string "----------------"
+string "  Introduza a   "
+string "   Password     "
+string "                "
+string "1) Confirmar    "
+string "2) Voltar       "
+
+;constantes
+
+;programa
+
+;rotinas
