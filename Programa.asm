bb1429685a50534b504ff97b58246792c3d43710
Pedro <jpcoutinhoalves@gmail.com>
2023-04-20 18:37:03 +0100

    Adição da rotina limpaPerifericos;
    Documentação de código

--- Programa.asm
+++ Programa.asm
@@ -1,6 +1,7 @@
 ;------ CONSTANTES ------;
 DisplayInicio equ 100h
 DisplayFim equ 16fh
+Opcao equ 200h
 
 ;------ PRODUTOS ------;
 
@@ -25,13 +26,13 @@ string "7) Cancelar     "
 string "----------------"
 
 MenuErro:
-string "                "
-string "                "
-string "    ATENCAO     "
-string "     OPCAO      "
-string "    ERRADA      "
-string "                "
-string "                "
+string "---------------!"
+string "!     OPCAO    !"
+string "!   INVALIDA   !"
+string "!--------------!"
+string "  PARA VOLTAR   "
+string "   INTRODUZA    "
+string "QUALQUER NUMERO "
 
 MenuStock:
 string "     Stock      "
@@ -49,20 +50,28 @@ mov r0, MenuInicio;
 call mostraDisplay;
 
 ;------ ROTINAS/FUNCOES ------;
-mostraDisplay: ; r0 indica o endereço do primeiro caracter do menu
-push r1 
-push r2
-push r3
+mostraDisplay:; escreve os menus no periférico de saída
+; input : r0 = endereço do menu a ser mostrado
+push r1; r1 = primeiro endereço do periférico de saida
+push r2; r2 = último endereço do periférico de saida
+push r3; r3 = caractere (em ASCII) no endereço r0
 mov r1, DisplayInicio
 mov r2, DisplayFim
 ciclo:
 mov r3, [r0]
 mov [r1], r3
-add r0, 2
-add r1, 2
+add r0, 2; r0 = r0 + 2
+add r1, 2; r1 = r1 + 2
 cmp r1, r2
-jlt ciclo
+jlt ciclo; r1 < DisplayFim?
 pop r3
 pop r2
 pop r1
+ret
+
+limpaPerifericos:
+push r0
+mov r0, Opcao; r0 = endereço do periferico de entrada
+mov [r0], 0; PER_EN = 0
+pop r0
 ret
